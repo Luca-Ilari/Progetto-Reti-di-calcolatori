74e0893fe733d5630cdceb88f581e328773de10a
TTTT-san <121175666+TTTT-san@users.noreply.github.com>
2024-03-13 17:18:36 +0100

    fix TransazioneTest.java

--- Client/NegozioCliente/src/test/java/it/itsrizzoli/model/TransazioneTest.java
+++ Client/NegozioCliente/src/test/java/it/itsrizzoli/model/TransazioneTest.java
@@ -17,6 +17,7 @@ class TransazioneTest {
         assertEquals(1, tmp.getQuantita());
         assertInstanceOf(Transazione.class, tmp);
     }
+
     @Test
     void newTransactionNegativeNumber() {
         var tmp = new Transazione(-12, -1);
@@ -24,34 +25,46 @@ class TransazioneTest {
         assertEquals(-12, tmp.getIdProdotto());
         assertEquals(-1, tmp.getQuantita());
     }
+
     @Test
     void createTransacitonMethod() {
         var tmp = Transazione.createTransaction(2);
         assertInstanceOf(Transazione.class, tmp);
         assertEquals(2, tmp.getIdProdotto());
-        assert(tmp.getQuantita() <= 20);
-        assert(tmp.getQuantita() > 0);
+        assert (tmp.getQuantita() > 0);
     }
+
     @Test
-    void creaListaTransazioniRandom(){
+    void creaListaTransazioniRandom() {
         int q = 3;
-        float prezzo[]=  new float[]{2.2f,24.2f,5.99f};
-        ArrayList<Prodotto> tmp = new ArrayList<Prodotto>(3);
-        for (int i = 0; i <   q ; i++) {
-            tmp.add(new Prodotto(i,"prod"+1, prezzo[i],10));
+        float prezzo[] = new float[]{2.2f, 24.2f, 5.99f};
+        ArrayList<Prodotto> prodotti = new ArrayList<Prodotto>(3);
+        for (int i = 0; i < q; i++) {
+            prodotti.add(new Prodotto(i, "prod" + i, prezzo[i], 10)); // corretto "prod" + 1 a "prod" + i
         }
 
-        var transazioni = Transazione.creaListaTransazioniRandom(tmp);
-        for (int i = 0; i < q; i++) {
-            assertEquals(tmp.get(i).getNome(), "prod"+1);
-            assertEquals(tmp.get(i).getPrezzo(), prezzo[i]);
-            assertEquals(tmp.get(i).getQuantitaDisponibile(), 10);
+        List<Transazione> transazioni = Transazione.creaListaTransazioniRandom(prodotti);
+
+        boolean idProdottoPresente = false;
+        for (Transazione transazione : transazioni) {
+            for (Prodotto prodotto : prodotti) {
+                if (transazione.getIdProdotto() == prodotto.getIdProdotto()) {
+                    idProdottoPresente = true;
+                    break;
+                }
+            }
+            if (!idProdottoPresente){
+                break;
+            }
         }
+
+        assert idProdottoPresente : "Nessun ID prodotto presente nelle transazioni";
     }
+
     @Test
-    void creaListaTransazioneArrayListVuoto(){
+    void creaListaTransazioneArrayListVuoto() {
         ArrayList<Prodotto> tmp = new ArrayList<Prodotto>(1);
         var transazioni = Transazione.creaListaTransazioniRandom(tmp);
-        assert(transazioni == null);
+        assert (transazioni == null);
     }
 }
