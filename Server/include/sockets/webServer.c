29823a5f4e245365e27b1f483ee453fddd938c50
Luca Ilari <luca.ilari@gmail.com>
2024-03-25 22:35:16 +0100

    webserver send index.html
    The webserver now responds with an html page in the folder of the server

--- Server/include/sockets/webServer.c
+++ Server/include/sockets/webServer.c
@@ -20,8 +20,53 @@
 
 #include "webServer.h"
 #include "socketFunctions.h"
+#include "../define.h"
 #include "../utils/timeStamp.h"
 #include "../utils/handleJson.h"
+
+int startWebServer(int *portno, int *sockfd, struct sockaddr_in *serv_addr){
+
+    timestamp();
+    printf("Web server starting on port:%d", *portno);
+
+#ifdef WIN32
+    WSADATA info;
+    if (WSAStartup(MAKEWORD(1, 1), &info) == SOCKET_ERROR) {
+        timestamp();
+        perror("ERROR, can't start webserver socket\n");
+    }
+#endif
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sockfd < 0){
+        timestamp();
+        perror("ERROR opening webserver socket\n");
+        return -1;
+    }
+    serv_addr->sin_family = AF_INET;
+    serv_addr->sin_addr.s_addr = INADDR_ANY;
+    serv_addr->sin_port = htons(*portno);
+    return 0;
+}
+
+char *buildHtmlResponse(FILE *htmlFile){
+    int htmlLen = 0;
+
+    while(fgetc(htmlFile) != EOF){htmlLen++;}//Get file length
+
+    fseek(htmlFile, 0, SEEK_SET); //reset file pointer to the start of the file
+
+    char *html = calloc(htmlLen, (htmlLen+1) * sizeof(char));
+    //memset(html, '\0', sizeof(html));
+
+    for (int i = 0; i < htmlLen; ++i) {
+        html[i] = (char)fgetc(htmlFile);
+        if (html[i] == NULL){
+            printf("a");
+        }
+    }
+    html[htmlLen] = '\0';
+    return html;
+}
 
 #ifdef WIN32
 DWORD WINAPI webServer(){
@@ -29,46 +74,46 @@ DWORD WINAPI webServer(){
 void *webServer(){
 #endif
     int sockfd, portno = 8080;
-    char buffer[1024];
+    char buffer[BUFFER_SIZE];
     struct sockaddr_in serv_addr;
-    timestamp();
-    printf("Web server starting on port:%d", portno);
+    FILE *htmlFile;
 
-    #ifdef WIN32
-    WSADATA info;
-    if (WSAStartup(MAKEWORD(1, 1), &info) == SOCKET_ERROR) {
+    int status = startWebServer(&portno, &sockfd, &serv_addr);
+    if(status < 0){
         timestamp();
-        perror("ERROR, can't start socket\n");
+        printf("Can't start webserver");
+        return -1;
     }
-    #endif
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd < 0){
-        timestamp();
-        perror("ERROR opening socket\n");
-    }
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
     if (bind(sockfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0){
-        perror("\nERROR on binding");
-        return 0;
+        perror("\nERROR on binding webserver socket");
+        return -1;
     }
+
+    htmlFile = fopen("./html/index.html", "r");
+    if(htmlFile == NULL){
+        timestamp();
+        printf("Can't find \"index.html\" webserver not started");
+        return -1;
+    }
+
     timestamp();
     printf("Web server started http://localhost:%d", portno);
+
     while(1){
-        memset(&serv_addr, 0, sizeof(serv_addr));
         int newsockfd = acceptNewConnection(sockfd);
-        memset(&buffer, 0, sizeof(buffer));
-        recv(newsockfd,buffer,1024-1,0);
-        //printf("%s\n", buffer);
-        memset(&buffer, 0, sizeof(buffer));
-        char *a = getProductJson();
-        int lenght = strlen(a) + strlen("<!DOCTYPE html><script> function autoRefresh() { window.location = window.location.href; } setInterval('autoRefresh()', 10000); </script>");
-        sprintf(buffer,"HTTP/1.1 200 OK\r\nContent-Length: %d\r\nServer: ReallyBadWebServer/1.0\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><script> function autoRefresh() { window.location = window.location.href; } setInterval('autoRefresh()', 10000); </script>%s", lenght, a);
-        sendToClient(newsockfd, buffer);
-        free(a);
-        a = NULL;
+        memset(buffer, '\0', sizeof(buffer));
+        recv(newsockfd,buffer,BUFFER_SIZE-1,0);
+        char *htmlResponse = buildHtmlResponse(htmlFile);
+
+        size_t len = strlen(htmlResponse);
+        memset(buffer, '\0', sizeof(buffer));
+        sprintf(buffer,"HTTP/1.1 200 OK\r\nContent-Length: %zu\r\nServer: ReallyBadWebServer/1.0\r\nContent-Type: text/html\r\n\r\n%s", len, htmlResponse);
+
+        sendToClient(newsockfd, buffer);//Send html page
+        free(htmlResponse);
+        htmlResponse = NULL;
+
         #ifdef WIN32
         closesocket(newsockfd);
         #else
@@ -80,4 +125,5 @@ void *webServer(){
     #else
     close(sockfd);
     #endif
+    return 0;
 }
